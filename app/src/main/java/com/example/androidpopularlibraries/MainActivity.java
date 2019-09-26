951f42e1c9e4e85ddf34c103f2ce82a24ab3ae68
mr.Blue <yashinsergey125@gmail.com>
2019-09-26 18:29:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidpopularlibraries/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.androidpopularlibraries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String baseUrl = "https://api.github.com";
+    private TextView mInfoTextView;
+    private ProgressBar progressBar;
+    private EditText editText;
+    private Button btnLoad;
+    private Retrofit retrofitAdapter = new Retrofit.Builder()
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+    RestApi api = retrofitAdapter.create(RestApi.class);
+
+    public interface RestApi{
+        @GET("users/{user}")
+        Single<RetrofitModel> getData(@Path("user") String user);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        btnLoad.setOnClickListener(this);
+    }
+
+    private void initViews() {
+        editText = findViewById(R.id.editText);
+        mInfoTextView = findViewById(R.id.tvLoad);
+        progressBar = findViewById(R.id.progressBar);
+        btnLoad = findViewById(R.id.btnLoad);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (!checkNetworkConnection()) return;
+
+        downloadOneUrl(editText.getText().toString());
+        hideKeyboard(this, view);
+    }
+
+    private boolean checkNetworkConnection() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        assert connectivityManager != null;
+        NetworkInfo networkinfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkinfo == null || !networkinfo.isConnected()) {
+            Toast.makeText(this, "Подключите интернет", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void downloadOneUrl(String request) {
+        api.getData(request).retry(2).subscribeOn(Schedulers.io()).subscribe(new SingleObserver<RetrofitModel>() {
+            @Override
+            public void onSubscribe(Disposable d) {}
+
+            @Override
+            public void onSuccess(RetrofitModel retrofitModel) {
+                mInfoTextView.setText(retrofitModel.getAvatarUrl());
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                e.printStackTrace();
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    public static void hideKeyboard(Context context, View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(INPUT_METHOD_SERVICE);
+        Objects.requireNonNull(imm).hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+}
