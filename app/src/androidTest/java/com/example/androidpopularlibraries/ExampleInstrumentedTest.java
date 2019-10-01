54e1f0aab22d8d1dab81fd229d132784168a9735
mr.Blue <yashinsergey125@gmail.com>
2019-10-01 14:13:43 +0300

    tests

--- app/src/androidTest/java/com/example/androidpopularlibraries/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/androidpopularlibraries/ExampleInstrumentedTest.java
@@ -2,11 +2,20 @@ package com.example.androidpopularlibraries;
 
 import android.content.Context;
 
+import androidx.room.Room;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.example.androidpopularlibraries.database.IRoomModelDao;
+import com.example.androidpopularlibraries.database.RoomDB;
+import com.example.androidpopularlibraries.model.RoomModel;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -17,6 +26,49 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    private static RoomDB database;
+    private IRoomModelDao usersDao;
+
+
+    @Before
+    public void createDb() throws Exception {
+        database = Room.inMemoryDatabaseBuilder(InstrumentationRegistry
+                .getInstrumentation().getContext(), RoomDB.class).build();
+        usersDao = database.dao();
+    }
+
+    @Test
+    public void whenInsetUserThenReadTheSameOne() throws Exception {
+        List<RoomModel> users = RoomTestHelper.createListOfUsers(1);
+        usersDao.insertAll(users);
+
+        List<RoomModel> roomDbList = usersDao.getAll();
+
+        assertEquals(1, roomDbList.size());
+        assertTrue(RoomTestHelper.listsAreIdentical(users, roomDbList));
+    }
+
+    @Test
+    public void whenInsertUsersThenReadThem() throws Exception {
+        List<RoomModel> users = RoomTestHelper.createListOfUsers(5);
+        usersDao.insertAll(users);
+        assertEquals(5, usersDao.getAll().size());
+    }
+
+    @Test
+    public void whenDeleteAllUsersThenReadThem() throws Exception {
+        List<RoomModel> users = RoomTestHelper.createListOfUsers(5);
+        usersDao.insertAll(users);
+        usersDao.deleteAll();
+        assertTrue(usersDao.getAll().isEmpty());
+    }
+
+    @After
+    public void closeDb() throws Exception {
+        database.close();
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
